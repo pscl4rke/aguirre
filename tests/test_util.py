ad47f7e405aa81fde43500bf6a3d77786797ee7d
P. S. Clarke <aguirre@pscl4rke.net>
2024-04-27 11:04:05 +0100

    Refactor tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -3,7 +3,7 @@
 from os.path import dirname, join
 import unittest
 
-from aguirre.util import guess_mime_type, load_from_tarball
+from aguirre import util
 
 
 def example_dir():
@@ -17,21 +17,21 @@ class TestTarballLowLevelAccess(unittest.TestCase):
         self.badpath = join(example_dir(), "not-an-archive.txt")
 
     def test_loading_a_good_file(self):
-        result = load_from_tarball(self.tarpath, "package/src/van.js")
+        result = util.load_from_tarball(self.tarpath, "package/src/van.js")
         self.assertEqual(len(result), 4917)
 
     def test_failing_on_a_bad_file(self):
         with self.assertRaises(KeyError):
-            load_from_tarball(self.tarpath, "src/van.js")
+            util.load_from_tarball(self.tarpath, "src/van.js")
 
     def test_failing_on_a_bad_archive(self):
         with self.assertRaises(Exception):
-            load_from_tarball(self.badpath, "package/src/van.js")
+            util.load_from_tarball(self.badpath, "package/src/van.js")
 
 
 class TestMimeTypes(unittest.TestCase):
 
     def test_guessing_the_mimetype(self):
-        self.assertEqual(guess_mime_type("/foo/thing.js"), "text/javascript")
-        self.assertEqual(guess_mime_type("/foo/thing.css"), "text/css")
-        self.assertEqual(guess_mime_type("/foo/thing.xyz"), "text/html")
+        self.assertEqual(util.guess_mime_type("/foo/thing.js"), "text/javascript")
+        self.assertEqual(util.guess_mime_type("/foo/thing.css"), "text/css")
+        self.assertEqual(util.guess_mime_type("/foo/thing.xyz"), "text/html")
