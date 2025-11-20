cba00c031bf61a2c2beba6f53fb3bd333a2f0dd8
P. S. Clarke <aguirre@pscl4rke.net>
2025-11-20 21:42:11 +0000

    Add tests for the flask integration

---
+++ tests/test_integrations_flask.py
@@ -0,0 +1,24 @@
+
+
+import unittest
+
+
+from flask import Flask
+APP = Flask("demo")
+
+import aguirre.integrations.flask as aguirre_flask
+APP.register_blueprint(aguirre_flask.create_blueprint("tests/examples"),
+                       url_prefix="/vendor")
+
+
+class TestHeaders(unittest.TestCase):
+
+    def test_present(self):
+        client = APP.test_client()
+        x = client.get("/vendor/vanjs-core@1.5.0/src/van.js")
+        self.assertTrue(x.status.startswith("200 "))
+
+    def test_absent(self):
+        client = APP.test_client()
+        x = client.get("/vendor/vanjs-core@1.5.0/src/missing.js")
+        self.assertTrue(x.status.startswith("404 "))
