6feeddcf47551bf70ec486f71e280c066c3e2acc
P. S. Clarke <aguirre@pscl4rke.net>
2025-12-14 23:02:20 +0000

    Introduce quart testing

---
+++ tests/test_integrations_quart.py
@@ -0,0 +1,24 @@
+
+
+import unittest
+
+
+from quart import Quart
+APP = Quart("demo")
+
+import aguirre.integrations.quart as aguirre_quart
+APP.register_blueprint(aguirre_quart.create_blueprint("tests/examples"),
+                       url_prefix="/vendor")
+
+
+class TestHeaders(unittest.IsolatedAsyncioTestCase):
+
+    async def test_present(self):
+        client = APP.test_client()
+        x = await client.get("/vendor/vanjs-core@1.5.0/src/van.js")
+        self.assertEqual(x.status_code, 200)
+
+    async def test_absent(self):
+        client = APP.test_client()
+        x = await client.get("/vendor/vanjs-core@1.5.0/src/missing.js")
+        self.assertEqual(x.status_code, 404)
